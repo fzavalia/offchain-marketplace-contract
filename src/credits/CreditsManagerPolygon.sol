fa0bffb7cd544e9419f5d148214d9ba0f4efec31
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-03-01 17:46:52 -0300

    fix: Move storage back to main contract

--- src/credits/CreditsManagerPolygon.sol
+++ src/credits/CreditsManagerPolygon.sol
@@ -14,11 +14,96 @@ import {IMarketplace} from "src/credits/interfaces/IMarketplace.sol";
 import {ILegacyMarketplace} from "src/credits/interfaces/ILegacyMarketplace.sol";
 import {ICollectionFactory} from "src/credits/interfaces/ICollectionFactory.sol";
 import {ICollectionStore} from "src/credits/interfaces/ICollectionStore.sol";
-import {CreditsManagerPolygonStorage} from "src/credits/CreditsManagerPolygonStorage.sol";
 
-contract CreditsManagerPolygon is CreditsManagerPolygonStorage, AccessControl, Pausable, ReentrancyGuard, NativeMetaTransaction {
+contract CreditsManagerPolygon is AccessControl, Pausable, ReentrancyGuard, NativeMetaTransaction {
     using ECDSA for bytes32;
     using SafeERC20 for IERC20;
+
+    /// @notice The role that can sign credits.
+    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+
+    /// @notice The role that can pause the contract.
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    /// @notice The role that can deny users from using credits.
+    bytes32 public constant DENIER_ROLE = keccak256("DENIER_ROLE");
+
+    /// @notice The role that can revoke credits.
+    bytes32 public constant REVOKER_ROLE = keccak256("REVOKER_ROLE");
+
+    /// @notice The role that can sign external calls.
+    bytes32 public constant EXTERNAL_CALL_SIGNER_ROLE = keccak256("EXTERNAL_CALL_SIGNER_ROLE");
+
+    /// @notice The role that can revoke external calls.
+    bytes32 public constant EXTERNAL_CALL_REVOKER_ROLE = keccak256("EXTERNAL_CALL_REVOKER_ROLE");
+
+    /// @notice Asset type for NFTs for the Marketplace Trade struct.
+    uint256 public constant ASSET_TYPE_ERC721 = 3;
+
+    /// @notice Asset type for collection items for the Marketplace Trade struct.
+    uint256 public constant ASSET_TYPE_COLLECTION_ITEM = 4;
+
+    /// @notice Whether a user is denied from using credits.
+    mapping(address => bool) public isDenied;
+
+    /// @notice Whether a credit has been revoked.
+    /// @dev The key is the hash of the credit signature.
+    mapping(bytes32 => bool) public isRevoked;
+
+    /// @notice The address of the MANA token.
+    IERC20 public immutable mana;
+
+    /// @notice The amount of MANA value used on each credit.
+    /// @dev The key is the hash of the credit signature.
+    mapping(bytes32 => uint256) spentValue;
+
+    /// @notice Maximum amount of MANA that can be credited per hour.
+    uint256 public maxManaCreditedPerHour;
+
+    /// @notice How much MANA has been credited this hour.
+    uint256 public manaCreditedThisHour;
+
+    /// @notice The hour of the last MANA credit.
+    uint256 public hourOfLastManaCredit;
+
+    /// @notice Whether primary sales are allowed.
+    bool public primarySalesAllowed;
+
+    /// @notice Whether secondary sales are allowed.
+    bool public secondarySalesAllowed;
+
+    /// @notice Whether bids are allowed.
+    bool public bidsAllowed;
+
+    /// @notice The address of the Marketplace contract.
+    address public immutable marketplace;
+
+    /// @notice The address of the Legacy Marketplace contract.
+    address public immutable legacyMarketplace;
+
+    /// @notice The address of the CollectionStore contract.
+    address public immutable collectionStore;
+
+    /// @notice The address of the CollectionFactory contract.
+    ICollectionFactory public immutable collectionFactory;
+
+    /// @notice The address of the CollectionFactoryV3 contract.
+    ICollectionFactory public immutable collectionFactoryV3;
+
+    /// @notice Tracks the allowed custom external calls.
+    mapping(address => mapping(bytes4 => bool)) public allowedCustomExternalCalls;
+
+    /// @notice Tracks the used external call signatures.
+    mapping(bytes32 => bool) public usedCustomExternalCallSignature;
+
+    /// @dev Value stored temporarily to check the validity of credits used for bids.
+    bytes32 internal tempBidCreditsSignaturesHash;
+
+    /// @dev Value stored temporarily to check the validity of max uncredited value used for bids.
+    uint256 internal tempMaxUncreditedValue;
+
+    /// @dev Value stored temporarily to check the validity of max credited value used for bids.
+    uint256 internal tempMaxCreditedValue;
 
     /// @notice The roles to initialize the contract with.
     /// @dev Mainly used to decrease the number of arguments in the constructor.
@@ -153,10 +238,7 @@ contract CreditsManagerPolygon is CreditsManagerPolygonStorage, AccessControl, P
         address _collectionStore,
         ICollectionFactory _collectionFactory,
         ICollectionFactory _collectionFactoryV3
-    )
-        CreditsManagerPolygonStorage(_mana, _marketplace, _legacyMarketplace, _collectionStore, _collectionFactory, _collectionFactoryV3)
-        EIP712("Decentraland Credits", "1.0.0")
-    {
+    ) EIP712("Decentraland Credits", "1.0.0") {
         _grantRole(DEFAULT_ADMIN_ROLE, _roles.owner);
 
         _grantRole(SIGNER_ROLE, _roles.signer);
@@ -180,6 +262,13 @@ contract CreditsManagerPolygon is CreditsManagerPolygonStorage, AccessControl, P
         _updatePrimarySalesAllowed(_primarySalesAllowed);
         _updateSecondarySalesAllowed(_secondarySalesAllowed);
         _updateBidsAllowed(_bidsAllowed);
+
+        mana = _mana;
+        marketplace = _marketplace;
+        legacyMarketplace = _legacyMarketplace;
+        collectionStore = _collectionStore;
+        collectionFactory = _collectionFactory;
+        collectionFactoryV3 = _collectionFactoryV3;
     }
 
     /// @notice Pauses the contract.
